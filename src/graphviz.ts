d7e99171a725976e2cf6d4b82862777e303f56ea
Anillc <i@anillc.cn>
2022-05-23 23:12:06 +0800

    add dotgif command

--- src/graphviz.ts
+++ src/graphviz.ts
@@ -1,7 +1,10 @@
 import { Context, Logger, segment } from 'koishi'
 import { exportToBuffer } from '@ts-graphviz/node'
+import { Page } from 'puppeteer-core'
 
 const logger = new Logger('graphviz')
+
+export const using = ['puppeteer']
 
 export function apply(ctx: Context) {
     ctx.command('graph <script:rawtext>')
@@ -25,6 +28,30 @@ export function apply(ctx: Context) {
                     ${script}
                 }
             `)
+        })
+    ctx.command('dotgif <...dots>')
+        .option('delay', '-d <delay:int>')
+        .option('duration', '-u <duration:int>')
+        .option('fps', '-f <fps:int>')
+        .option('circle', '-c')
+        .check(({ options }) => {
+            if (options.fps > 100) return 'fps 过大'
+        })
+        .action(async ({ options }, ...dots) => {
+            let page: Page
+            try {
+                const query = JSON.stringify({ ...options, dots })
+                page = await ctx.puppeteer.page()
+                await page.goto(`https://d3-graphviz-to-gif.vercel.app?${encodeURIComponent(query)}`)
+                const gif = await page.waitForSelector('#gif')
+                const url = await gif.evaluate(e => (e as any).src)
+                return segment.image('base64://' + url.slice(22))
+            } catch (e) {
+                logger.error(e)
+                return '渲染时发生错误: ' + e
+            } finally {
+                page?.close()
+            }
         })
 }
 
