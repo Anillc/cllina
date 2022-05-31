79e283f1b8a3ba400b81596af9b44e1df03f51f9
Anillc <i@anillc.cn>
2022-06-01 02:51:48 +0800

    add regex commands

--- koishi.dhall
+++ koishi.dhall
@@ -81,6 +81,7 @@ in {
         `./src/forward`  = {=},
         `./src/do`       = {=},
         `./src/type`     = {=},
+        `./src/regex`    = {=},
         `./src/dynamic`  = {=},
         `./src/graphviz` = {=},
         `./src/ppt` = {
---
+++ src/regex.ts
@@ -0,0 +1,41 @@
+import { Context } from 'koishi'
+
+export function apply(ctx: Context) {
+    ctx.command('regex <regex:string> <text:rawtext>')
+        .option('global', '-g global')
+        .option('ignore-case', '-i ignore case')
+        .option('multiline', '-m multiline')
+        .option('stringify', '-s stringify')
+        .action(({ options }, regex, text) => {
+            try {
+                let flags = ''
+                if (options['global']) flags += 'g'
+                if (options['ignore-case']) flags += 'i'
+                if (options['multiline']) flags += 'm'
+                const match = new RegExp(regex, flags).exec(text)
+                if (options.stringify) {
+                    return JSON.stringify(match, null, 2)
+                } else {
+                    return match?.[1] || '未匹配到结果'
+                }
+            } catch (e) {
+                return String(e)
+            }
+        })
+    ctx.command('replace <regex:string> <replacer:string> <text:rawtext>')
+        .alias('sed')
+        .option('global', '-g global')
+        .option('ignore-case', '-i ignore case')
+        .option('multiline', '-m multiline')
+        .action(({ options }, regex, replacer, text) => {
+            try {
+                let flags = ''
+                if (options['global']) flags += 'g'
+                if (options['ignore-case']) flags += 'i'
+                if (options['multiline']) flags += 'm'
+                return text.replace(new RegExp(regex, flags), replacer)
+            } catch (e) {
+                return String(e)
+            }
+        })
+}
