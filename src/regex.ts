2226459f72c4e10eb6e311bc2bb922774ba9d4d9
Anillc <i@anillc.cn>
2022-06-01 13:34:38 +0800

    enhance regex

--- src/regex.ts
+++ src/regex.ts
@@ -2,6 +2,7 @@ import { Context } from 'koishi'
 
 export function apply(ctx: Context) {
     ctx.command('regex <regex:string> <text:rawtext>')
+        .alias('re')
         .option('global', '-g global')
         .option('ignore-case', '-i ignore case')
         .option('multiline', '-m multiline')
@@ -12,11 +13,11 @@ export function apply(ctx: Context) {
                 if (options['global']) flags += 'g'
                 if (options['ignore-case']) flags += 'i'
                 if (options['multiline']) flags += 'm'
-                const match = new RegExp(regex, flags).exec(text)
+                const match = new RegExp(`(${regex})`, flags).exec(text)
                 if (options.stringify) {
                     return JSON.stringify(match, null, 2)
                 } else {
-                    return match?.[1] || '未匹配到结果'
+                    return match?.[2] || match?.[1] || '未匹配到结果'
                 }
             } catch (e) {
                 return String(e)
