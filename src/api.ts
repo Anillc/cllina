a49a68299d95d8e82d7353b7a58535e23f444619
Anillc <i@anillc.cn>
2022-06-28 22:32:31 +0800

    add prompt api

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import { Context, Logger, User } from 'koishi'
+import { Context, Logger, User, Session } from 'koishi'
 import { nanoid } from 'nanoid'
 import cors from '@koa/cors'
 
@@ -11,6 +11,26 @@ declare module 'koishi' {
 declare module 'koa' {
     interface DefaultContext {
         user: User
+        platform: string
+    }
+}
+
+export interface ApiConfig {
+    platform: string
+}
+
+interface Response {
+    code: number
+    message: string
+    data: any
+}
+
+function response(res: Partial<Response>): Response {
+    return {
+        code: 200,
+        message: 'success',
+        data: null,
+        ...res
     }
 }
 
@@ -18,11 +38,15 @@ export const name = 'api'
 
 const logger = new Logger('api')
 
-export function apply(ctx: Context) {
+export function apply(ctx: Context, config: ApiConfig) {
+    config = {
+        platform: 'onebot',
+        ...config
+    }
     ctx.model.extend('user', {
         apiToken: 'string'
     })
-    ctx.using(['database'], api)
+    ctx.using(['database'], ctx => api(ctx, config))
     ctx.private().command('api.register', { authority: 3 })
         .userFields(['apiToken'])
         .option('revoke', '-r 撤销原有 token 并重新生成')
@@ -36,39 +60,79 @@ export function apply(ctx: Context) {
         })
 }
 
-function api(ctx: Context) {
+function api(ctx: Context, config: ApiConfig) {
     const router = ctx.router.prefix('/api/:token')
         .use(cors())
         .use(async (koaCtx, next) => {
             const { token } = koaCtx.params
+            let { platform } = koaCtx.query
+            if (!platform) {
+                platform = config.platform
+            }
+            if (Array.isArray(platform)) {
+                koaCtx.body = response({ code: 403, message: 'multiple platforms passed' })
+                return
+            }
             try {
                 const user = (await ctx.database.get('user', { apiToken: token }))?.[0]
                 if (!user) return
                 koaCtx.user = user
+                koaCtx.platform = platform
                 return next()
             } catch (e) {
                 logger.error(e)
-                koaCtx.status = 500
-                koaCtx.body = String(e)
+                koaCtx.body = response({ code: 500, message: String(e) })
             }
         })
-    router.get('/send/:platform', async koaCtx => {
-        const { user, params, query } = koaCtx
-        const { msg } = query
-        const uid = user[params.platform] as string
-        const bot = ctx.bots.find(bot => bot.platform === params.platform)
-        if (!uid || !bot) {
-            koaCtx.status = 404
-            koaCtx.body = '请检查 uid 和 platform 是否正确。'
+    router.get('/send', async koaCtx => {
+        const { user, query, platform } = koaCtx
+        const { message } = query
+        const uid = user[platform] as string
+        const bot = ctx.bots.find(bot => bot.platform === platform)
+        if (!uid || !bot || !message) {
+            koaCtx.body = response({ code: 403, message: 'check params' })
             return
         }
         try {
-            const messageId = await bot.sendPrivateMessage(uid, Array.isArray(msg) ? msg.join(' ') : msg)
+            const messageId = await bot.sendPrivateMessage(uid, Array.isArray(message) ? message.join(' ') : message)
             koaCtx.status = 200
-            koaCtx.body = JSON.stringify(messageId)
+            koaCtx.body = response({ data: messageId })
         } catch (e) {
-            koaCtx.status = 500
-            koaCtx.body = String(e)
+            koaCtx.body = response({ code: 500, message: String(e) })
+        }
+    })
+    router.get('/prompt', async koaCtx => {
+        const { user, query, platform } = koaCtx
+        const { message, timeout } = query
+        const uid = user[platform] as string
+        const bot = ctx.bots.find(bot => bot.platform === platform)
+        if (!uid || !bot) {
+            koaCtx.body = response({ code: 403, message: 'check uid or platform' })
+            return
+        }
+        const session = new Session(bot, {
+            userId: user[platform],
+            channelId: `private:${user[platform]}`
+        })
+        try {
+            let messageId = []
+            if (message) {
+                messageId = await session.send(Array.isArray(message) ? message.join(' ') : message)
+            }
+            const parsedTimeout = parseInt(Array.isArray(timeout) ? timeout[0] : timeout)
+            const res = await session.prompt(parsedTimeout ? parsedTimeout : undefined)
+            if (!res) {
+                koaCtx.body = response({ code: 206, message: 'timeout', data: { ask: messageId }})
+                return
+            }
+            koaCtx.body = response({
+                data: {
+                    ask: messageId,
+                    response: res,
+                }
+            })
+        } catch (e) {
+            koaCtx.body = response({ code: 500, message: String(e) })
         }
     })
 }
